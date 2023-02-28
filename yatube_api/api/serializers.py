e43df29c5c35bc4892965240e80a27fbcdd23a6c
Роман Иванов <zaregi@mail.ru>
2023-03-01 01:23:06 +0300

    First Rewiew

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,12 +1,31 @@
+import base64
+
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+from django.core.files.base import ContentFile
+
+from posts.models import Comment, Post, Group, Follow, User
 
 
-from posts.models import Comment, Post
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            file_format, img_code = data.split(';base64,')
+            ext = file_format.split('/')[-1]
+            data = ContentFile(base64.b64decode(img_code), name='temp.' + ext)
+        return super().to_internal_value(data)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         fields = '__all__'
@@ -21,3 +40,49 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate(self, data):
+        if data.get('following') == self.context.get('request').user:
+            raise serializers.ValidationError(
+                'Нельзя подписываться на самого себя'
+            )
+        return data
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписаны на этого пользователя'
+            )
+        ]
+    # Не совсем понял, зачем нам тут надо UniqueTogetherValidator описывать,
+    # если мы в модели Follow указали UniqueConstraint, но без этого pytest
+    # не принимал.Или сериализатор берет из модели только поля, а что в Meta
+    # описанно игнорирует?
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('post',)
